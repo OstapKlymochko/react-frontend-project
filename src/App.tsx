7baebb9d28c3865a8e7b3f0d9859c3d3f9861ace
OstapKlymochko <klymochkoo@gmail.com>
2023-02-23 18:22:37 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from "./hooks";
+import {moviesActions} from "./redux";
 
 
 const App: FC = () => {
-
+    let dispatch = useAppDispatch();
+    let {details} = useAppSelector(state => state.movieReducer);
+    useEffect(()=>{
+        dispatch(moviesActions.getById({id:288097}));
+    },[dispatch]);
+    console.log(details);
     return (
         <div>
             App
